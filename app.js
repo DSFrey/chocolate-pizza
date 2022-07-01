fabfdeed7229851565912866b0f5a59673aca9db
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-07-01 16:31:21 -0500

    Flesh out html, add css, start js

---
+++ app.js
@@ -0,0 +1,10 @@
+const ingredients = document.getElementsByClassName("ingredients");
+
+for (let i = 0; i < ingredients.length; i++) {
+  for (let ii = 0; ii < ingredients[i].children.length; ii++) {
+    ingredients[i].children[ii].addEventListener('change',function(i,ii) {
+      ingredients[i].children[ii].style.textdecoration = 
+    }
+    )
+  }  
+}
