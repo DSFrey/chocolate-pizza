87d2337c8c97faea8a0425381196ed5e7c05852e
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-07-01 16:41:47 -0500

    Finish js function

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const ingredients = document.getElementsByClassName("ingredients");
 
 for (let i = 0; i < ingredients.length; i++) {
   for (let ii = 0; ii < ingredients[i].children.length; ii++) {
-    ingredients[i].children[ii].addEventListener('change',function(i,ii) {
-      ingredients[i].children[ii].style.textdecoration = 
-    }
-    )
+    ingredients[i].children[ii].addEventListener('change',function() {
+      if (ingredients[i].children[ii].children[0].checked) {
+        ingredients[i].children[ii].style.textDecoration = "line-through"
+      } else {
+        ingredients[i].children[ii].style.textDecoration = "none"
+      }
+    })
   }  
 }
